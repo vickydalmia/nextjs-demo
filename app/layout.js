5d69c6dd1bcccf9269ebc930617450eaf14b7409
Vicky <vicky8394@gmail.com>
2024-06-02 15:57:49 +0530

    added tailwind

---
+++ app/layout.js
@@ -0,0 +1,21 @@
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+export const metadata = {
+  title: "The Wild Oasis",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
+        <header>
+          <Logo />
+          <Navigation />
+        </header>
+        <main>{children}</main>
+        <footer>Copyright by The Wild Oasis</footer>
+      </body>
+    </html>
+  );
+}
